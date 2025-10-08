e886b188171f6c5f2edc647edbb1a238e1ef1e58
toki-nt <tokinantenainaraboba@gmail.com>
2025-10-08 11:01:45 +0300

    Update dbConnex.js

--- config/dbConnex.js
+++ config/dbConnex.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
-//const MONGODB_URI = "mongodb+srv://Tokinantenaina:Ny001300@cluster0.awd9epp.mongodb.net/myFirstDB"
 mongoose
-  .connect(`${MONGODB_URI}`)
+  .connect(MONGODB_URI)
   .then(() => {
     console.log("connected to MongoDb");
   })
